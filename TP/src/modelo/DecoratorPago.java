1bd945c56dc91e2aa9339129d5a57da8180b2488
FedeGarcia00 <fedegarciamdq@gmail.com>
2023-04-25 20:54:06 -0300

    .

--- TP/src/modelo/DecoratorPago.java
+++ TP/src/modelo/DecoratorPago.java
@@ -2,4 +2,10 @@ package modelo;
 
 public abstract class DecoratorPago implements IMonitoreo {
 	private IMonitoreo encapsulado;
+
+	public DecoratorPago(IMonitoreo encapsulado) {
+		super();
+		this.encapsulado = encapsulado;
+	}
+	
 }
