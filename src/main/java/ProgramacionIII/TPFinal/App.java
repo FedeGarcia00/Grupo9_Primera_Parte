4bfef77638c260fd5e0aa52c3841dfb20b2ee583
FedeGarcia00 <fedegarciamdq@gmail.com>
2023-04-27 15:55:14 -0300

    Reorganizacion Maven

---
+++ src/main/java/ProgramacionIII/TPFinal/App.java
@@ -0,0 +1,13 @@
+package ProgramacionIII.TPFinal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
